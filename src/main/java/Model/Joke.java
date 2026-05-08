7870d83ad8ab0d2c050878a02e05e26308f53716
Magdeli-Potgieter <magdelipotgieter.mp@gmail.com>
2026-05-08 14:09:25 +0200

    Joke class fixed

--- src/main/java/Model/Joke.java
+++ src/main/java/Model/Joke.java
@@ -1,24 +1,3 @@
-//package Model;
-//
-//import lombok.*;
-//
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@ToString
-//@EqualsAndHashCode
-//@Data
-//
-//public class Joke {
-//    //Instance variables:
-//    private int jokeId;
-//    private int creatorId;
-//    private String jokeText;
-//    private String status; //Status can be: 'pending', 'approved', or 'rejected'.
-//}
-
 package Model;
 
 import lombok.*;
@@ -31,13 +10,11 @@ import lombok.*;
 @ToString
 @EqualsAndHashCode
 @Data
+
 public class Joke {
-    // Instance variables:
+    //Instance variables:
     private int jokeId;
     private int creatorId;
     private String jokeText;
-    private String setup;
-    private String punchline;
-    private String category;
-    private String status; // Status can be: 'pending', 'approved', or 'rejected'.
+    private String status; //Status can be: 'pending', 'approved', or 'rejected'.
 }
